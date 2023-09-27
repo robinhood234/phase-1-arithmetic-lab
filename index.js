52ae94860c8044c19be82df6b04649080b74a86a
robin nganga smith <saintofcookies.2021@gmail.com>
2023-09-27 10:52:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 7.874007874011811019685034448812;
+let num2 = 7.874007874011811019685034448812;
+const multiply = num1 * num2;
+let random = Math.floor(Math.random()*10)+1;
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max(5, 10, 15, 20);
+
